47ada26f16e5cb4016a974ba088caa672e28b39c
Leah Moser <leahmoser@Leahs-MBP.home>
2021-04-22 21:51:11 -0700

    Added some more of the functionality for the reverse exercise still need validaiton

--- hw3/03-reverse.js
+++ hw3/03-reverse.js
@@ -1,6 +1,19 @@
 document.querySelector('#reverse').onclick = function () {
   // Add your code here validate that it is 8 digits 
   //put an error code for when it is not 
-  n = n + "";
-	return n.split("").reverse().join("");
+  let u_input = document.getElementById('input');
+  let output = document.getElementById('message');
+
+  let saved = u_input.value;
+
+  if(saved.length != 8){
+    output.className = 'text-danger';
+    output.textContent = 'Error: Please enter an 8 digit number';
+    return;
+  }
+
+  let reversed = u_input.value.split("").reverse().join("");
+  output.className = 'text-success';
+  output.textContent = saved + "-->" + reversed;
+
 };
