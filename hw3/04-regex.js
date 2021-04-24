f2a245c9f69c64dcee4de637a7aa1a761b19eff1
Leah Moser <leahmoser@Leahs-MBP.home>
2021-04-24 10:54:27 -0700

    Went a different route for the validation. Could not get the event listner to work.

--- hw3/04-regex.js
+++ hw3/04-regex.js
@@ -1,1 +1,22 @@
-// Add your code here
+
+function checkEmail(email) {
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    if (!reg.test(email)) return false;
+    return true;
+}
+
+function email_val() {
+
+    let email = document.getElementById('input').value;
+    let output = document.getElementById('message');
+    if(checkEmail(email))
+    {
+        output.className = 'text-success';
+        output.textContent = 'Thank you. This is a valid email address.'
+    }
+    else 
+    {
+        output.className = 'text-danger';
+        output.textContent = 'Error: Please enter a valid email address.'
+    }
+};
