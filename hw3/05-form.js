06189c185d2f8006dbc74d1bfdcef119998eec96
Leah Moser <leahmoser@Leahs-MBP.home>
2021-04-24 13:31:32 -0700

    Added some elements for app and form

--- hw3/05-form.js
+++ hw3/05-form.js
@@ -1,3 +1,20 @@
 function handleSubmit(event) {
   // Add your code here
 }
+let form = document.querySelector('form');
+  form.addEventListener("submit", event =>{
+    event.preventDefault();
+    console.group('========Form submission========')
+    console.log('Name:  ', form.elements.name.value);
+    console.log('Email ', form.elements.email.value);
+    if(form.elements.comments.value.length > 0){
+      console.log('Feedback: ', form.elements.comments.value);
+    }
+    if(form.elements.signup.checked){
+      console.log('Newletter: Yes, I would like to join the newsletter.');
+    }
+    else{
+      console.log('Newsletter: No, I would not like to join the newsletter');
+    }
+    console.groupEnd();
+  });
--- hw3/06-api.js
+++ hw3/06-api.js
@@ -1,7 +1,24 @@
 const url = 'https://restcountries.eu/rest/v2/all';
 
+let app = document.querySelector("#app");
+
 let getData = (url) => {
-  // Add your code here
+  // Add your code herei
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((country) => {
+        console.log('${country.name[0]}- ${country.population[0]}');
+        let element = document.createElement("div");
+        element.textContent = '${country.name[0]} - ${country.population[0]}';
+        app.append(element);
+      });
+    })
+   .catch((error) => console.log("Error", error))
+   .finally(()=>console.log("Run no matter what"));
+
+
 };
 
 getData(url);
