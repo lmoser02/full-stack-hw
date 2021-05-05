06eda4c9fae70d2667425d5f432471a09be08d4d
Leah Moser <leahmoser@Leahs-MBP.home>
2021-05-05 11:13:04 -0700

    Added the first exercise

--- hw4/01-node-routing.js
+++ hw4/01-node-routing.js
@@ -43,7 +43,48 @@ const server = http.createServer((req, res) => {
     res.end();
   }
 
-  // Add your code here
+  else if (req.method === 'GET' && req.url === '/welcome') {
+    console.log(`${req.method} - ${req.url}`);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Howdy! This is the welcome page</h1>');
+    res.end();
+  }
+  else if (req.url === '/redirect') {
+    res.writeHead(302, {Location: "/redirected"});
+    res.end();
+  }
+  else if (req.url === '/redirected') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>You have been being redirected</h1>');
+    res.end();
+  }
+  else if (req.method === 'GET' && req.url === '/cache') {
+    res.setHeader('Cache-control','Max-Age=86400');
+    res.write('<h1>This resource has been cached</h1>');
+    res.end();
+  }
+  else if (req.method === 'GET' && req.url === '/cookie') {
+    res.setHeader('Set-Cookie','hello=world');
+    res.write('<h1>Cookies....yummmmm</h1>');
+    res.end();
+  }
+  else if(req.url === '/check-cookies'){
+    res.writeHead(200, {"Content-Type": 'text/html'});
+    let result = 'No';
+    if(req.headers.cookie === 'hello=world'){
+      result = 'Yes';
+    }
+    res.write(`Presence of hello=world cookie on site: ${result}`);
+    res.end();
+  }
+  
+  else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.write('<h1>404: Page not found</h1>');
+    res.end();
+  }
+
+
 });
 
 server.listen(port, () => {
