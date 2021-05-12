96bcc8adabba6919e3927d63d0f36a52750a2ed8
Leah Moser <leahmoser@Leahs-MBP.home>
2021-05-12 15:06:37 -0700

    finished the chat app for socket.io

--- hw5/03-socketio/index.js
+++ hw5/03-socketio/index.js
@@ -10,8 +10,23 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
+  });
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
