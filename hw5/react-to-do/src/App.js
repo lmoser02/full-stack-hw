cd990f71dbb53c555722ef4cdaa3e71f782adb88
Leah Moser <leahmoser@Leahs-MBP.home>
2021-05-12 13:46:10 -0700

    added stuff for the second react walk through

---
+++ hw5/react-to-do/src/App.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./App.css";
+
+function Todo({ todo }) {
+  return (
+    <div className="todo">
+      {todo.text}
+    </div>
+  );
+};
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function App() {
+  const [todos, setTodos] = React.useState([
+    { text: "Learn about React" },
+    { text: "Meet friend for lunch" },
+    { text: "Build really cool todo app" }
+  ]);
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
